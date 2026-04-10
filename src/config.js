63749f1b5a03e7c20e48fd883f13b8be16ce96c1
Salam Plinth <55528362+drKriP@users.noreply.github.com>
2026-04-10 11:38:55 +0530

    feat: change email

--- src/config.js
+++ src/config.js
@@ -1,63 +1,62 @@
 
 const config = {
-    hero: {
-        name: "Salam Plinth",
-        company: "i-exceed",
-        bio: "Developer | Tech Enthusiast | Open Source Contributor",
-        socials: [
-          { name: "GitHub", link: "https://github.com/drKriP"},
-          { name: "LinkedIn", link: "https://www.linkedin.com/in/salamplinth" },
-        ],
-      },
-    about: {
-        description:
-          "I am a passionate developer with experience in full-stack development, microservices, and scalable applications.",
-        skills: ["JavaScript", "Java", "GO", "C/C++", "Python",
-          "Java Spring", "Node.js", "React", "Next.js", 
-          "MongoDB", "OracleDB", "MySQL",
-           "Docker", "Kubernetes", "CyberSecurity"],
-      },
-      projects: [
-        {
-          name: "Resume Website",
-          description: "A personal portfolio developed in Next.js with github actions for CI/CD deployed in azure static web apps.",
-          github: "https://github.com/drKriP/my-portfolio",
-        },
-        {
-          name: "SyncD3V",
-          description: "An automation tool for syncing development environments.",
-          github: "https://github.com/drKriP/SyncD3V",
-        },
-        {
-          name: "Computer Vision",
-          description: "A personal project to learn computer vision.",
-          github: "https://github.com/drKriP/computer-vision",
-        },
-        {
-          name: "Validator.js",
-          description: "An open-source library for validating data which I'm contributing to.",
-          github: "https://github.com/drKriP/validator.js",
-        },
-        {
-          name: "Go Service",
-          description: "A personal project to learn GO.",
-          github: "https://github.com/drKriP/go-service",
-        },
-        {
-          name: "Quiz Management Microservice",
-          description: "A college project for scalable service to demonstrate microservices.",
-          github: "https://github.com/BITS-WILP-Scalable-Service-Group-25/quiz-management",
-        },
-      ],
-    contact: {
-      email: "plinthluwang@gmail.com",
+  hero: {
+    name: "Salam Plinth",
+    company: "i-exceed",
+    bio: "Developer | Tech Enthusiast | Open Source Contributor",
+    socials: [
+      { name: "GitHub", link: "https://github.com/drKriP" },
+      { name: "LinkedIn", link: "https://www.linkedin.com/in/salamplinth" },
+    ],
+  },
+  about: {
+    description:
+      "I am a passionate developer with experience in full-stack development, microservices, and scalable applications.",
+    skills: ["JavaScript", "Java", "GO", "C/C++", "Python",
+      "Java Spring", "Node.js", "React", "Next.js",
+      "MongoDB", "OracleDB", "MySQL",
+      "Docker", "Kubernetes", "CyberSecurity"],
+  },
+  projects: [
+    {
+      name: "Resume Website",
+      description: "A personal portfolio developed in Next.js with github actions for CI/CD deployed in azure static web apps.",
+      github: "https://github.com/drKriP/my-portfolio",
     },
-    contactInfo: {
-      email: "salamplinth@luwang.site",
-      phone: "+91 8730092055",
-      address: "Bengaluru, India",
+    {
+      name: "SyncD3V",
+      description: "An automation tool for syncing development environments.",
+      github: "https://github.com/drKriP/SyncD3V",
     },
-  };
-  
-  export default config;
-  
+    {
+      name: "Computer Vision",
+      description: "A personal project to learn computer vision.",
+      github: "https://github.com/drKriP/computer-vision",
+    },
+    {
+      name: "Validator.js",
+      description: "An open-source library for validating data which I'm contributing to.",
+      github: "https://github.com/drKriP/validator.js",
+    },
+    {
+      name: "Go Service",
+      description: "A personal project to learn GO.",
+      github: "https://github.com/drKriP/go-service",
+    },
+    {
+      name: "Quiz Management Microservice",
+      description: "A college project for scalable service to demonstrate microservices.",
+      github: "https://github.com/BITS-WILP-Scalable-Service-Group-25/quiz-management",
+    },
+  ],
+  contact: {
+    email: "plinthluwang@gmail.com",
+  },
+  contactInfo: {
+    email: "me@plinthluwang.in",
+    phone: "+91 8730092055",
+    address: "Bengaluru, India",
+  },
+};
+
+export default config;
